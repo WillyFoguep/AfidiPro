03eabff130b4a30830f337609e8a37b3660cc267
WillyFoguep <63255177+WillyFoguep@users.noreply.github.com>
2026-01-18 19:58:52 -0500

    auth with google

--- android/app/src/main/java/com/afidipro/app/MainActivity.java
+++ android/app/src/main/java/com/afidipro/app/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         WebView webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        // Définir un User-Agent compatible avec Google OAuth
+        String userAgent = "Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
+        webView.getSettings().setUserAgentString(userAgent);
         webView.loadUrl("https://www.afidipro.com");
         
     }
